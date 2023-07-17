ef02eac881b1ac20d2def1e28d9f14bf07e93d9b
miles-i-hilton <139785270+miles-i-hilton@users.noreply.github.com>
2023-07-17 16:23:01 +0100

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -1,11 +1,7 @@
 import random
 num=[]
 for n in range(100):
-<<<<<<< HEAD
-    nu=random.randint(1, 2)
-=======
     nu=random.randint(1, 2000)
->>>>>>> seperate-printed-numbers
     num.append(nu)
 print("100 randomly generated numbers:")
 print(num)
