259d154f911d4a0180a9be0893023fea3d9d3742
Miles Hilton <mileshilton2007@icloud.com>
2023-07-17 15:04:49 +0100

    added text

--- sorter.py
+++ sorter.py
@@ -3,8 +3,10 @@ num=[]
 for n in range(100):
     nu=random.randint(1, 2000)
     num.append(nu)
+print("100 randomly generated numbers:")
 print(num)
 print()
 num.sort()
+print("Sorted numbers in order of smallest to largest:")
 for item in num:
     print(item)
